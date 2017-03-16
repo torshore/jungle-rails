8262e1f66720ccd4c529630b2d927d08d763bea5
Tory Shoreman <toryshoreman@gmail.com>
2017-03-16 13:43:52 +0000

    Categories page and drop down working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all.order(created_at: :desc)
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
