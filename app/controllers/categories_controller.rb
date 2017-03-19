82b73278d426f615d4da2c87d8a34a0e159e081c
Tory Shoreman <toryshoreman@gmail.com>
2017-03-19 23:30:22 +0000

    Reviews in tables

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all.order(created_at: :desc)
+  end
 
   def show
     @category = Category.find(params[:id])
